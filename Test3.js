1700e1a61d3685fad9b082d5355ded0fc8915335
Iuliia Tokarevskaia <iuliia.tokarevskaia@socialbakers.com>
2020-12-30 14:44:32 +0300

    Проверено

--- Test3.js
+++ Test3.js
@@ -3,3 +3,7 @@
 // arr.reverse();
 // let perevorot = arr.join("");
 // console.log(perevorot)
+
+// Всё сделано верно, но можно записать всё то же самое более коротко и не создавать лишние переменные:
+let str = 'Hello';
+let reverseStr = str.split('').reverse.join('');
